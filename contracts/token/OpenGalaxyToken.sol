c612c73023b883aabe6b474bf34f66fca0aca9f3
Alexey Okhotnikov <deathpressence@gmail.com>
2021-12-15 18:38:10 +0300

    feat(token): OpenGalaxyToken contract added

---
+++ contracts/token/OpenGalaxyToken.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract OpenGalaxyToken is ERC20, ERC20Capped, ERC20Permit, ERC20Votes {
+  constructor()
+    ERC20("Open Galaxy", "GLX")
+    ERC20Capped(1000000000 * 10**decimals())
+    ERC20Permit("Open Galaxy")
+  {
+    _mint(msg.sender, cap());
+  }
+
+  // The following functions are overrides required by Solidity.
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override(ERC20, ERC20Votes) {
+    super._afterTokenTransfer(from, to, amount);
+  }
+
+  function _mint(address to, uint256 amount)
+    internal
+    override(ERC20, ERC20Capped, ERC20Votes)
+  {
+    super._mint(to, amount);
+  }
+
+  function _burn(address account, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+  {
+    super._burn(account, amount);
+  }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,25 +5,12 @@ require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
 require("solidity-coverage")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
 const accounts = {
   mnemonic:
     process.env.MNEMONIC ||
     "test test test test test test test test test test test test",
   count: 20,
 }
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
